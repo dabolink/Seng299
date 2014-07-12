a1bdcf4b3965c90dadf876f562ba1ec437e187ab
dabolink <dabolink@uvic.ca>
2014-07-12 00:37:28 -0700

    Added User and School to DB
    hardcoded student and school added

--- server/test.js
+++ server/test.js
@@ -1,18 +1,49 @@
 var mongoose = require('mongoose');
 
+var FN = 'Daniel';
+var LN = 'Bolink';
+var DoB = '08-14-1993';
+var g = 'male';
+var Username = 'dabolink';
+var Password = '1234';
+var email = 'dabolink@gmail.ca';
+var School = 'University of Victoria';
+
+
+
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function(){});
 mongoose.connect('mongodb://DigitalCoffee:1234@ds041208.mongolab.com:41238/seng299');
-//ds041208.mongolab.com:41238/seng299');
 
-var movieSchema = new mongoose.Schema({
-	title: { type: String}
+var School = new mongoose.Schema({
+	School: { type: String}
 	});
-var Movie = mongoose.model('Movie',movieSchema);
-var thor = new Movie({
-	title: 'thor'});
-thor.save(function(err, thor){
+
+var UserSchema = new mongoose.Schema({
+	FirstName: { type: String},
+	LastName: { type: String},
+	DateOfBirth: { type: Date},
+	gender: { type: String},
+	UserName: { type: String},
+	Password: { type: String},
+	EMail: { type: String},
+	School: { type: String},
+	});
+var User = mongoose.model('User', UserSchema);
+
+var test = new User({
+	FirstName: FN,
+	LastName: LN,
+	DateOfBirth: DoB,
+	gender: 'Male',
+	UserName: Username,
+	Password: Password,
+	EMail: email,
+	School: School,
+	});
+	
+test.save(function(err, test){
 	if(err) return console.error(err);
-	console.dir(thor);
+	console.dir(test);
 });
