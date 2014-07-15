c2d825fbc487c77b90d0ecb288648fecc4ebb77f
DigitalCoffee <a.hansen00@gmail.com>
2014-07-14 23:16:10 -0700

    Trying to get some MongoDB stuff going.
    ...It's not, though. I'm not even certain I'm connecting to the DB. D:

--- expresso.js
+++ expresso.js
@@ -4,6 +4,33 @@ var express = require('express'),
     https = require('https'),
 	fs = require('fs');
 
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+
+	var UserSchema = new mongoose.Schema({
+		FirstName: { type: String},
+		LastName: { type: String},
+		DateOfBirth: { type: Date},
+		gender: { type: String},
+		UserName: { type: String},
+		Password: { type: String},
+		EMail: { type: String},
+		School: { type: String},
+	});
+
+	var SchoolSchema = new mongoose.Schema({
+		School: { type: String}
+	});
+
+	var User = mongoose.model('User', UserSchema);
+	var School = mongoose.model('School', SchoolSchema);
+});
+
+mongoose.connect('mongodb://DigitalCoffee:1234@ds041208.mongolab.com:41238/seng299');
+//while (mongoose.connection.readyState != 1) {}
+console.log('MongoDB connection ready-state:  '+ mongoose.connection.readyState);
+
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
